f29afa5c49cccb865ad4cdad443f102ff2e544d0
MinhNguyenCr <minh.dox.555@gmail.com>
2025-08-28 09:07:22 +0700

    minh_forgot-password

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,15 @@ import ForgotPassword from './components/auth/forgotPasswordComponent/ForgotPass
 import VerifyOtp from './components/auth/verifyOtp/VerifyOtpComponent';  // Import VerifyOtp component
 import Register from './components/auth/registerComponent/RegisterComponent';
 import HomePage from './components/homePageComponent/HomePage';
+import ResetPassword from './components/auth/forgotPasswordComponent/ResetPassword'; // Import ResetPassword component
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/login" element={<Login />} />  {/* Đăng nhập */}
-        <Route path="/forgot-password" element={<ForgotPassword />} /> 
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} /> 
+        {/* <Route path="/verify-otp-reset" element={<VerifyOtp />} /> */}
         <Route path="/register" element={<Register />} /> 
         <Route path="/verify-otp" element={<VerifyOtp />} /> 
         <Route path="/home" element={<HomePage />} />{/* Thêm route cho trang Verify OTP */}
